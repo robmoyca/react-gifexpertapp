8da1a78fcecd5fba159cc6c004c0d40719cb5177
Roberto Moya <rmoya@tradestation.com>
2020-07-11 00:27:41 -0600

    Docs agregado

---
+++ docs/precache-manifest.f5ad298a5b2867f419aaf43a5a81765b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82964632e00a120e10cd1b8157da8019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584b70d757227a86ac01",
+    "url": "/static/css/main.59e39a88.chunk.css"
+  },
+  {
+    "revision": "e751102e7f9fbd0f2827",
+    "url": "/static/js/2.010b0ea3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.010b0ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584b70d757227a86ac01",
+    "url": "/static/js/main.7b44a8da.chunk.js"
+  },
+  {
+    "revision": "abcbd34042998abbb7fb",
+    "url": "/static/js/runtime-main.7d3d4d70.js"
+  }
+]);
